90d338d79ddc2df1319e5fdec6f71cd1891ffdeb
sametyorgunn <asametyorgun60@gmail.com>
2024-09-28 01:33:04 +0300

    rating

--- UI/Controllers/ProductController.cs
+++ UI/Controllers/ProductController.cs
@@ -12,7 +12,6 @@ namespace UI.Controllers
 		{
 			_productService = productService;
 		}
-
 		public async Task<IActionResult> Index()
 		{
 			var products = await _productService.GetLastTwelveProduct();
