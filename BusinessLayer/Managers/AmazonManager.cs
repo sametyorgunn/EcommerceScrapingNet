fa3b878e168c7dc6ecc61ff307864f001b80c642
sametjekirdek <samet.yorgun@jekirdek.com>
2024-11-23 15:16:28 +0300

    hide browser and scrape

--- BusinessLayer/Managers/AmazonManager.cs
+++ BusinessLayer/Managers/AmazonManager.cs
@@ -34,14 +34,16 @@ namespace BusinessLayer.Managers
         public async Task<ScrapingResponseDto> GetProductAndCommentsAsync(GetProductAndCommentsDto request)
 		{
 			var options = new ChromeOptions();
-            //options.AddArgument("--headless");
-            //options.AddArgument("--disable-gpu");
-            //options.AddArgument("--window-size=1920,1080");
-            //options.AddArgument("--no-sandbox");
-            //options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("window-size=1920x1080");
+            options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
 
-            
-			using (IWebDriver driver = new ChromeDriver(options))
+
+
+            using (IWebDriver driver = new ChromeDriver(options))
 			{
 				var jsExecutor = (IJavaScriptExecutor)driver;
 				driver.Navigate().GoToUrl("https://www.amazon.com.tr/");
